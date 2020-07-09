6545ae9f7342a6ae9af0c8386839d45e30a0231d
Aayush_Neupane <neupane0403@gmail.com>
2020-07-09 17:28:36 +0545

    insertion sort

---
+++ sorting/InsertionSort.java
@@ -0,0 +1,33 @@
+package sorting;
+import utility.RandNumber;
+public class InsertionSort {
+	
+	public static void sort(int arr []){
+		int n=arr.length-1;
+		for(int i=1;i<=n;i++) {
+			int j=i;
+			while( j>0 &&(arr[j]<arr[j-1])  ) {
+				swap(arr,j,j-1);
+
+				j--;
+			}
+		}
+
+	}
+	private static void swap(int [] arr,int i,int j) {
+		int temp=arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+	}
+	
+	//test client
+	public static void main(String [] args) {
+		int size=50;
+		int []arr=new int[size];
+		RandNumber.generateRandomArray(arr, size);
+		sort(arr);
+		console.log("insertion sorted array");
+		console.log(arr);
+	}
+
+}
--- utility/RandNumber.java
+++ utility/RandNumber.java
@@ -3,7 +3,7 @@ package utility;
 import java.util.Random;
 
 public class RandNumber {
-	private  static void generateRandomArray (int [] arr,int size) {
+	public  static void generateRandomArray (int [] arr,int size) {
 		Random rand=new Random();
 		for (int i=0;i<size;i++) {
 			arr[i]=rand.nextInt(size-1);
